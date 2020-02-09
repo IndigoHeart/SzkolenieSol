a1b37acdb1a771f1e3dd6ee270857e7aa47a9f14
indigoHeart <rafalbudny7@gmail.com>
2020-02-09 21:18:50 +0100

    Init database - migration +
    Init azure based database

--- SzkolenieSol/Szkolenie/Startup.cs
+++ SzkolenieSol/Szkolenie/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Szkolenie.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Szkolenie
 {
@@ -26,6 +28,7 @@ namespace Szkolenie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:ProductDatabase:ConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
