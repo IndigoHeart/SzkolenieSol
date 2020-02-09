099f2095555c99994daa16ae97c1d28495e6e797
indigoHeart <rafalbudny7@gmail.com>
2020-02-09 21:18:50 +0100

    Init database - migration +
    Init azure based database

---
+++ SzkolenieSol/Szkolenie/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Szkolenie.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public float UserRate { get; set; }
+        public int NumberOfPages { get; set; }
+    }
+}
