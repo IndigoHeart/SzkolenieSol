82683023eb262c8954f95bedf2b9120b64c2fb0b
indigoHeart <rafalbudny7@gmail.com>
2020-02-12 02:02:18 +0100

    Xunit

--- SzkolenieSol/XUnitProductTest/UnitTest1.cs
+++ SzkolenieSol/XUnitProductTest/UnitTest1.cs
@@ -1,4 +1,7 @@
 using System;
+using Szkolenie.Controllers;
+using Szkolenie.Data;
+using Szkolenie.Models;
 using Xunit;
 
 namespace XUnitProductTest
@@ -6,9 +9,16 @@ namespace XUnitProductTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanAddProduct()
         {
+            //Arrange
+            var p = new Product { Name = "Test name", Author = "Test author", ReleaseDate = DateTime.Now, UserRate = 2, NumberOfPages = 123 };
 
+            //Act
+            var context = new ApplicationDbContext();
+            var repo = new ProductRepository(context);
+            var prodcontroler = new ProductsController(productRepository);
+            //Assert
         }
     }
 }
